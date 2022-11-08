d50dc6a8c5271b82b3b81ea195ebcf97808b5fef
Melinatexeira <valeriamallom@gmail.com>
2022-11-08 15:39:57 -0300

    Proyecto2

---
+++ incidencia.php
@@ -0,0 +1,12 @@
+ <?php
+       $Usuario = $_POST["User"];
+       $Departemento = $_POST["Depto"];
+       $Descripcion = $_POST["desc_incidencia"];
+       $Contraseña = $_POST["Password"];
+
+       echo "Usuario ".$Usuario;
+       echo "</br>Departamento " .$Departemento;
+       echo "</br>Descripcion " .$Descripcion;
+       echo "</br>Contraseña " .$Contraseña;
+    ?>
+ 
